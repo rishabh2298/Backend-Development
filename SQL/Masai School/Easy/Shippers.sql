c9227433f982f38bfbb635ba0d998457cf3e9d61
rishabh2298 <rishabhmishra2298@gmail.com>
2024-03-19 20:58:14 +0530

    MIN(), MAX()

--- SQL/Masai School/Easy/Shippers.sql
+++ SQL/Masai School/Easy/Shippers.sql
@@ -8,3 +8,16 @@ SELECT
 FROM
  Shippers;
  
+
+
+--  2. ATOMIC
+
+-- Print the least and the highest sale_price amongst products sold by the Company ATOMIC.
+
+SELECT
+ MIN(sale_price), MAX(sale_price)
+FROM
+ Products
+WHERE
+ Brand = 'ATOMIC';
+ 
