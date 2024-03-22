5d874793aca3382256137957b80e498f99e6b68f
rishabh2298 <rishabhmishra2298@gmail.com>
2024-03-22 22:14:37 +0530

    Easy - MIN(), MAX(), DATE_FORMAT()

--- SQL/Masai School/Easy/Shippers.sql
+++ SQL/Masai School/Easy/Shippers.sql
@@ -21,3 +21,20 @@ FROM
 WHERE
  Brand = 'ATOMIC';
  
+
+
+--  3. 08-08-2021 Orders
+
+-- Print all details of orders that were placed on the 8th of August 2021.
+-- Sort the result set in ascending order of Order ID.
+-- Date format for orderdate is 'YYYY-MM-DD'.
+
+SELECT
+ *
+FROM
+ Orders
+WHERE
+ DATE_FORMAT(OrderDate, '%Y-%m-%d') = '2021-08-08'
+ORDER BY
+ OrderID ASC;
+ 
