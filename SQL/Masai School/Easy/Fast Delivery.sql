a59edcd245c79796a1e115a7a21173adf0f9a024
rishabh2298 <rishabhmishra2298@gmail.com>
2024-04-06 22:17:05 +0530

    DATEDIFF(), COUNT(), INNERJOIN

--- SQL/Masai School/Easy/Fast Delivery.sql
+++ SQL/Masai School/Easy/Fast Delivery.sql
@@ -19,4 +19,28 @@ ORDER BY
 
 -- 2. 5 Products
 
+-- Identify the Number of Orders placed by each Customer, where the Customer has ordered exactly 5 Unique Products in each order 
+-- (irrespective of the individual product quantity).
+-- Print Customer ID, First Name, Last Name, Count of Such orders placed by the Customers.
+-- Sort the result set in ascending order of Customer ID.
 
+SELECT
+   c.CustomerID, c.FirstName, c.LastName, COUNT(o.OrderID)
+FROM
+   Customers AS c INNER JOIN Orders AS o
+   ON c.CustomerID = o.CustomerID
+   INNER JOIN (
+      SELECT
+          OrderID, COUNT(DISTINCT ProductID) AS product_count
+      FROM
+          OrderDetails
+      GROUP BY
+          OrderID
+      HAVING
+          product_count = 5 
+   ) AS od
+   ON o.OrderID = od.OrderID
+GROUP BY
+   c.CustomerID, c.FirstName, c.LastName
+ORDER BY
+   c.CustomerID ASC;
