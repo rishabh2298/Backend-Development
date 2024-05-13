56f3ba7ec7443d00e58d8ba3efdce3bd41256ab6
rishabh2298 <rishabhmishra2298@gmail.com>
2024-05-13 15:47:43 +0530

    JS - Data collection completed

--- JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
+++ JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
@@ -2,7 +2,25 @@
 let form = document.querySelector("form");
 form.addEventListener("submit", createTable);
 
+let students_Detail = [];
 
-function createTable(){
-    
+function createTable(event){
+
+    event.preventDefault();
+
+    let student_Date = {
+        img : document.querySelector("#image").value,
+        name : document.querySelector("#name").value,
+        batch : document.querySelector("#batch").value,
+        dsa_Score : document.querySelector("#dsa_score").value,
+        skillathon_score : document.querySelector("#skillathon_score").value,
+        coding_score : document.querySelector("#coding_score").value,
+    }
+
+
+
+    // console.table(student_Date);
+    students_Detail.push(student_Date);
+
+    form.reset();
 }
