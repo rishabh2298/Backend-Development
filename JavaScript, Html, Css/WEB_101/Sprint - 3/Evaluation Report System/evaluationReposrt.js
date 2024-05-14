7e1d39d0f4d14cf4c6ae34439d3c6ca5b2aee51d
rishabh2298 <rishabhmishra2298@gmail.com>
2024-05-14 20:17:44 +0530

    JS - content store and upload in table successfully

--- JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
+++ JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
@@ -7,6 +7,9 @@ let students_Detail = [];
 function createTable(event){
 
     event.preventDefault();
+    
+    let box = document.querySelector("#container");
+    box.innerHTML = ""; 
 
     let student_Date = {
         img : document.querySelector("#image").value,
@@ -21,12 +24,13 @@ function createTable(event){
     console.table(students_Detail);
 
     students_Detail.forEach(function(){
-        let box = document.querySelector("#container");
-        
+
         let tr = document.createElement("tr");
 
         let td1 = document.createElement("td");
-        let img = document.createAttribute("imgage");
+        let img = document.createElement("img");
+        img.setAttribute("src",student_Date.img);
+        td1.append(img);
 
         let td2 = document.createElement("td");
         td2.innerText = student_Date.name;
@@ -41,8 +45,24 @@ function createTable(event){
         td5.innerText = student_Date.skillathon_score;
 
         let td6 = document.createElement("td");
-        td5.innerText = student_Date.coding_score;
+        td6.innerText = student_Date.coding_score;
 
+        let td7 = document.createElement("td");
+        let percentage = (Number(student_Date.dsa_Score) + Number(student_Date.skillathon_score) + Number(student_Date.coding_score))/30 * 100;
+        td7.innerText = percentage.toFixed(3) + " %";
+
+        let td8 = document.createElement("td");
+        if(percentage <= 50){
+            td8.innerText = "ASYNC";
+            td8.style.backgroundColor = "red";
+        }
+        else{
+            td8.innerText = "REGULAR";
+            td8.style.backgroundColor = "green";
+        }
+        td8.style.color = "white";
+
+        tr.append(td1,td2,td3,td4,td5,td6,td7,td8);
         box.append(tr);
     });
 
