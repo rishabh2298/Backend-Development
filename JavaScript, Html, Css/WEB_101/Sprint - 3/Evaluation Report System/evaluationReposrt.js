e6b8598fff72888fe80a3119d76500d8d6ca926f
rishabh2298 <rishabhmishra2298@gmail.com>
2024-05-14 19:45:27 +0530

    JS - data capture from FORM and store in array

--- JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
+++ JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
@@ -17,10 +17,35 @@ function createTable(event){
         coding_score : document.querySelector("#coding_score").value,
     }
 
-
-
-    // console.table(student_Date);
     students_Detail.push(student_Date);
+    console.table(students_Detail);
+
+    students_Detail.forEach(function(){
+        let box = document.querySelector("#container");
+        
+        let tr = document.createElement("tr");
+
+        let td1 = document.createElement("td");
+        let img = document.createAttribute("imgage");
+
+        let td2 = document.createElement("td");
+        td2.innerText = student_Date.name;
+
+        let td3 = document.createElement("td");
+        td3.innerText = student_Date.batch
+
+        let td4 = document.createElement("td");
+        td4.innerText = student_Date.dsa_Score;
+
+        let td5 = document.createElement("td");
+        td5.innerText = student_Date.skillathon_score;
+
+        let td6 = document.createElement("td");
+        td5.innerText = student_Date.coding_score;
+
+        box.append(tr);
+    });
+
 
     form.reset();
 }
