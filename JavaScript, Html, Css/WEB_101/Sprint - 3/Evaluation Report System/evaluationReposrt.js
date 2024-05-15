da0e549517488b3abd9459bdec67a6d412e889a4
rishabh2298 <rishabhmishra2298@gmail.com>
2024-05-15 23:38:13 +0530

    JS - Status created, dynamic data updated

--- JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
+++ JavaScript, Html, Css/WEB_101/Sprint - 3/Evaluation Report System/evaluationReposrt.js
@@ -23,32 +23,34 @@ function createTable(event){
     students_Detail.push(student_Date);
     console.table(students_Detail);
 
-    students_Detail.forEach(function(){
+    students_Detail.forEach(function(elem,index,arr){
 
         let tr = document.createElement("tr");
 
         let td1 = document.createElement("td");
         let img = document.createElement("img");
-        img.setAttribute("src",student_Date.img);
+        img.setAttribute("src",elem.img);
+        img.style.height = "50px";
+        img.style.width = "40px";
         td1.append(img);
 
         let td2 = document.createElement("td");
-        td2.innerText = student_Date.name;
+        td2.innerText = elem.name;
 
         let td3 = document.createElement("td");
-        td3.innerText = student_Date.batch
+        td3.innerText = elem.batch
 
         let td4 = document.createElement("td");
-        td4.innerText = student_Date.dsa_Score;
+        td4.innerText = elem.dsa_Score;
 
         let td5 = document.createElement("td");
-        td5.innerText = student_Date.skillathon_score;
+        td5.innerText = elem.skillathon_score;
 
         let td6 = document.createElement("td");
-        td6.innerText = student_Date.coding_score;
+        td6.innerText = elem.coding_score;
 
         let td7 = document.createElement("td");
-        let percentage = (Number(student_Date.dsa_Score) + Number(student_Date.skillathon_score) + Number(student_Date.coding_score))/30 * 100;
+        let percentage = (Number(elem.dsa_Score) + Number(elem.skillathon_score) + Number(elem.coding_score))/30 * 100;
         td7.innerText = percentage.toFixed(3) + " %";
 
         let td8 = document.createElement("td");
@@ -61,6 +63,7 @@ function createTable(event){
             td8.style.backgroundColor = "green";
         }
         td8.style.color = "white";
+        td8.style.padding = "5px";
 
         tr.append(td1,td2,td3,td4,td5,td6,td7,td8);
         box.append(tr);
