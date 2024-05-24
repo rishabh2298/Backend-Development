ad4da6f8eec5d12105cd17c1d7cce8b0744597f9
rishabh2298 <rishabhmishra2298@gmail.com>
2024-05-24 22:49:36 +0530

    Easy - SUM() OVER (PARTITION ORDER ROWS PRECEDING CURRENT) Rooling_Sum

--- SQL/Masai School/Easy/Monthly Counts.sql
+++ SQL/Masai School/Easy/Monthly Counts.sql
@@ -18,3 +18,22 @@ GROUP BY
    MONTH(OrderDate)
 ORDER BY
    customer_count ASC;
+
+
+
+-- 2. 3 Day Rolling Sum Preceding
+
+-- Write a query to find the 3 day rolling sum of the Total order amount of all the orders for each shipper sorted in ascending order of Order ID.
+
+-- Print Order ID,Shipper ID, Total order amount and 3 day rolling sum. (Find the 3 day rolling sum by taking 2 previous rows and the current row.)
+
+-- Sort the result in ascending order of Shipper ID, for records with same shipper ID, sort them in ascending order of Order ID.
+
+SELECT
+   OrderID, ShipperID, Total_order_amount,
+   SUM(Total_order_amount) OVER (PARTITION BY ShipperId ORDER BY OrderID ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS Rolling_Sum
+FROM
+   Orders
+ORDER BY
+   ShipperId ASC, OrderId ASC;
+   
