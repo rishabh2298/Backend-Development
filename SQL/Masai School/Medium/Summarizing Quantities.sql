8e327a95708cfd0b15ce8aad87e3a62612ece02d
rishabh2298 <rishabhmishra2298@gmail.com>
2024-05-31 23:03:18 +0530

    Medium - Multiple Custom columns with HelpTable - WITH HelpTable AS ()

--- SQL/Masai School/Medium/Summarizing Quantities.sql
+++ SQL/Masai School/Medium/Summarizing Quantities.sql
@@ -35,3 +35,48 @@ GROUP BY
 ORDER BY
    year_ ASC, quarter_ ASC;
 
+
+
+-- 2. Summarizing Country Orders
+
+-- Print the following Pivot Table
+-- The values in the matrix represent the total number of orders ordered from the different countries in the database through the different years and quarters. (Consider OrderDate)
+-- Sort the result in ascending order of Year, for records with the same year, sort them in ascending order of Quarter.
+
+-- Your Output:
+
+-- |    |   year_ |   quarter_ |   India |   United States |   Australia |   Switzerland |
+-- |---:|--------:|-----------:|--------:|----------------:|------------:|--------------:|
+-- |  0 |    2020 |          1 |     nan |               7 |           2 |             1 |
+-- |  1 |    2020 |          2 |       2 |              42 |           9 |            13 |
+-- |  2 |    2020 |          3 |      16 |              85 |           5 |            16 |
+-- |  3 |    2020 |          4 |      37 |              89 |           8 |            16 |
+-- |  4 |    2021 |          1 |      42 |              99 |          15 |            28 |
+-- |  5 |    2021 |          2 |      74 |             111 |          45 |            55 |
+-- |  6 |    2021 |          3 |     101 |             110 |         127 |            69 |
+-- |  7 |    2021 |          4 |     153 |              91 |          86 |            96 |
+
+
+WITH HelpTable AS(
+   SELECT
+      YEAR(o.OrderDate) AS year_, QUARTER(o.OrderDate) AS quarter_, c.Country AS country_name, COUNT(*) as total_order_count
+   FROM
+      Customers AS c INNER JOIN Orders AS o ON c.CustomerId = o.CustomerId
+   GROUP BY
+      year_, quarter_, country_name
+)
+
+
+SELECT
+   year_, quarter_,
+   SUM(CASE WHEN country_name = 'India' THEN total_order_count END) AS 'India',
+   SUM(CASE WHEN country_name = 'United States' THEN total_order_count END) AS 'United States',
+   SUM(CASE WHEN country_name = 'Australia' THEN total_order_count END) AS 'Australia',
+   SUM(CASE WHEN country_name = 'Switzerland' THEN total_order_count END) AS 'Switzerland'
+FROM
+   HelpTable
+GROUP BY
+   year_, quarter_
+ORDER BY
+   year_, quarter_;
+   
