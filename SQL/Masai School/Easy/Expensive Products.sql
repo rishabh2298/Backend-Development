bad691f6b31ecab33d54b1e8eb3246139380ce6e
rishabh2298 <rishabhmishra2298@gmail.com>
2024-07-08 23:12:54 +0530

    Easy - ROUND(), AVG()

--- SQL/Masai School/Easy/Expensive Products.sql
+++ SQL/Masai School/Easy/Expensive Products.sql
@@ -16,3 +16,25 @@ WHERE
 ORDER BY
    productID ASC;
 
+
+
+/*
+
+Calculate the average discount percentage on products of each brand belonging to the bakeware sub category.
+Print Brand name and their average percentage discount in nearest integer value.(e.g. - 77.80 will be 78 and 77.10 will be 77)
+DISCOUNT = ((MARKET_PRICE - SALE_PRICE)/MARKET_PRICE) X 100
+Sort the result in desceding order of Average discount value.
+
+*/
+
+SELECT
+   brand, ROUND(AVG(((Market_price - Sale_price)/market_price)*100)) AS avg_discount
+FROM
+   Products
+WHERE
+  Sub_Category = 'bakeware'
+GROUP BY
+  brand
+ORDER BY
+   avg_discount DESC;
+
