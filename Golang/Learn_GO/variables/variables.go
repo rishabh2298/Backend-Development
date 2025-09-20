19a8bb51caee4210b4ebb9fd277feb27b4dcac1a
rishabh2298 <rishabhmishra2298@gmail.com>
2025-09-20 23:17:45 +0530

    variables declaration and initialization

---
+++ Golang/Learn_GO/variables/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string
+	name = "pankaj"
+	fmt.Println("Name :", name)
+
+	var name2 string = "rajesh"
+	fmt.Println("Name-2 :", name2)
+
+	var name3 = "harish"
+	fmt.Println("Name-3 :", name3)
+
+	name4 := "Praiyadarshan"
+	fmt.Println("Name-4 :", name4)
+
+	status := false
+	fmt.Println("Status :", status)
+
+}
