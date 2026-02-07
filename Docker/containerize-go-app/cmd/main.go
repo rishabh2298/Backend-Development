958bf3dd2d257262fc81b897f40bdad4dbde0ef2
rishabh2298 <rishabhmishra2298@gmail.com>
2026-02-08 03:05:41 +0530

    app port:8080, container run port:8085

--- Docker/containerize-go-app/cmd/main.go
+++ Docker/containerize-go-app/cmd/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"containerize-go-app/internal/routers"
+	"fmt"
+	"os"
 )
 
 func main() {
 
 	router := routers.SetupRouter()
 
-	err := router.Run(":8080")
+	prtNo := os.Getenv("RUN_PORT")
+	if prtNo == "" {
+		prtNo = "8080"
+	}
 
+	fmt.Printf("=============== port No : %s ==================\n", prtNo)
+
+	err := router.Run(fmt.Sprintf(":%s", prtNo))
 	if err != nil {
 		panic(err)
 	}
